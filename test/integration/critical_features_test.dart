cae0fefc3d198e1ef610e877801745661f597f94
Mahtab Ali <m.ali@oxinus.io>
2026-02-23 11:53:34 +0400

    added test

--- test/integration/critical_features_test.dart
+++ test/integration/critical_features_test.dart
@@ -29,7 +29,7 @@ void main() {
           projectId: 'test',
           apiKey: 'test-key',
         );
-68
+
         // Test with a mock expired token (JWT with exp in past)
         const expiredToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MTYyMzkwMjJ9.4Adcj0pRR8ypXG5Wy';
 
